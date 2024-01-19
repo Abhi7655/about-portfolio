5e4c5bfc96117d2fe38f19f4d88c46de9001db99
Sharma Abhishek (Consultant) <abhishek.sharma@consultant.volvo.com>
2024-01-20 00:11:03 +0530

    first commit

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,73 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+          A seasoned web developer with a passion for crafting seamless and visually appealing digital experiences. With an impressive track record spanning 8 years, I have honed my skills in front-end development, becoming adept at turning ideas into pixel-perfect, user-friendly interfaces
+          If you're seeking a web developer who combines technical expertise with creativity and a results-driven mindset, I would be thrilled to discuss how my experience can contribute to the success of your projects.
+          Let's build the future of the web together! 💻✨
+
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "B.Tech in Information Technology",
+      content: (
+        <>
+          <p>
+           Graduated in Informationa nd Technology from Bharati VidyaPeeth College Of Engineering 
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "Higher School diploma",
+      content: (
+        <>
+          <p>
+           Comepleted my Schooling from SRML Higher Secondary
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2021 Senior Front End Developer",
+      content: (
+        <>
+          <p>
+            Volvo Sweden – Volvo, Automobile Company responsible for developing various class of vehicles , developing the GDSP (GLOBAL DIAGNOSTIC SERVICE PLATFORM)
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2020 Front End Developer",
+      content: (
+        <>
+          <p>
+          NCS Singapore - Worked for Airport Customs Departament, Helped deploy system enhancements to improve field operations
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2017-12 Junior Developer",
+      content: (
+        <>
+          <p>
+            Worked on verious project ranging from financial to pharma domain with reputed banks and Companies like TBS,RBC ,Merck etc
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
---
+++ src/skills/skillsData.js
@@ -0,0 +1,57 @@
+const skills = {
+  1: [
+    {
+      title: "Angular",
+      level: 5,
+    },
+    {
+      title: "Polymer",
+      level: 4,
+    },
+    {
+      title: "JavaScript",
+      level: 5,
+    },
+    {
+      title: "TypeScript",
+      level: 4,
+    },
+    {
+      title: "HTML",
+      level: 5,
+    },
+    {
+      title: "CSS",
+      level: 5,
+    },
+    {
+      title: "SASS",
+      level: 2,
+    }
+ 
+  ],
+  2: [
+    {
+      title: "Node.js",
+      level: 3,
+    },
+    {
+      title: "MongoDB",
+      level: 2,
+    },
+    {
+      title: "Git",
+      level: 4,
+    },
+    {
+      title: "SQL",
+      level: 1,
+    },
+    {
+      title: "Express.js",
+      level: 3,
+    },
+  ],
+};
+
+export default skills;
